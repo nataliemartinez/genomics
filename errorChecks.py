4693800117dd0b4d127f64ff490095e552b4f4be
nickgarza <ngarza1@jhu.edu>
2019-11-12 17:32:40 -0500

    added more sample data

--- errorChecks.py
+++ errorChecks.py
@@ -1,6 +1,7 @@
 import sys 
 import glob
 
+""" Convert ascii char to quality score """
 def phred33_to_q(qual):
     return ord(qual)-33
 
@@ -19,14 +20,14 @@ def listOccurrences(f, t, reads_dict):
                 break  # end of file
             name = first_line[1:].rstrip() # line 1 contains name
             seq = fh.readline().rstrip() # line 2 contains sequence
-            fh.readline()  # ignore line starting with +
+            fh.readline()  # line 3 contains '+' so we skip
             qual = fh.readline().rstrip() # line 4 contains quality
 
             qual_sum = 0
             for i in range(len(qual)): # get total of quality scores
                 qual_sum += phred33_to_q(qual[i])
 
-            if qual_sum/len(qual) >= t:  # if avg quality > threshold add to dict {file: {seq: [occ,...]}}
+            if qual_sum/len(qual) >= t:  # if avg quality > threshold add to dict {seq: {file: [occ,...]}}
                 file_occ_dict = reads_dict.get(seq, {})
                 file_occ = file_occ_dict.get(fh.name, []) 
                 file_occ.append(name)
@@ -35,15 +36,17 @@ def listOccurrences(f, t, reads_dict):
     
     return reads_dict
     
-
+"""
+Read all the fastqs in our directory and build map {seq: {file: [occ,...]}}
+"""
 def readFiles(): 
-    k_mer_counts = {} 
-    files = sys.argv[1:]
+    k_mer_counts = {} # map of {seq: {file: [occ,...]}}
+
     for filename in glob.glob('*.fastq'):
         # do stuff
         listOccurrences(filename, 5, k_mer_counts)
 
     for k,v in k_mer_counts.items():
-        print(v)
+        print(k)
 
 readFiles()
